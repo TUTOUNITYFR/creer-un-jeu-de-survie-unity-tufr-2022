26dd78b6eff51e12940c68141d615bcc27d4fd9d
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2022-06-06 19:57:16 +0200

    Épisode 05 : Actions et types d'items

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -16,6 +16,38 @@ public class Inventory : MonoBehaviour
 
     const int InventorySize = 24;
 
+    [Header("Action Panel References")]
+
+    [SerializeField]
+    private GameObject actionPanel;
+
+    [SerializeField]
+    private GameObject useItemButton;
+
+    [SerializeField]
+    private GameObject equipItemButton;
+
+    [SerializeField]
+    private GameObject dropItemButton;
+
+    [SerializeField]
+    private GameObject destroyItemButton;
+
+    private ItemData itemCurrentlySelected;
+
+    [SerializeField]
+    private Sprite emptySlotVisual;
+
+    [SerializeField]
+    private Transform dropPoint;
+
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     private void Start()
     {
         RefreshContent();
@@ -42,6 +74,16 @@ public class Inventory : MonoBehaviour
 
     private void RefreshContent()
     {
+        // On vide tous les slots / visuels
+        for (int i = 0; i < inventorySlotsParent.childCount; i++)
+        {
+            Slot currentSlot = inventorySlotsParent.GetChild(i).GetComponent<Slot>();
+
+            currentSlot.item = null;
+            currentSlot.itemVisual.sprite = emptySlotVisual;
+        }
+
+        // On peuple le visuel des slots selon le contenu réel de l'inventaire
         for (int i = 0; i < content.Count; i++)
         {
             Slot currentSlot = inventorySlotsParent.GetChild(i).GetComponent<Slot>();
@@ -55,5 +97,69 @@ public class Inventory : MonoBehaviour
     {
         return InventorySize == content.Count;
     }
-    
+
+    public void OpenActionPanel(ItemData item, Vector3 slotPosition)
+    {
+        itemCurrentlySelected = item;
+
+        if(item == null)
+        {
+            actionPanel.SetActive(false);
+            return;
+        }
+
+        switch(item.itemType)
+        {
+            case ItemType.Ressource:
+                useItemButton.SetActive(false);
+                equipItemButton.SetActive(false);
+                break;
+            case ItemType.Equipment:
+                useItemButton.SetActive(false);
+                equipItemButton.SetActive(true);
+                break;
+            case ItemType.Consumable:
+                useItemButton.SetActive(true);
+                equipItemButton.SetActive(false);
+                break;
+        }
+
+        actionPanel.transform.position = slotPosition;
+        actionPanel.SetActive(true);
+    }
+
+    public void CloseActionPanel()
+    {
+        actionPanel.SetActive(false);
+        itemCurrentlySelected = null;
+    }
+
+    public void UseActionButton()
+    {
+        print("Use item : " + itemCurrentlySelected.name);
+        CloseActionPanel();
+    }
+
+    public void EquipActionButton()
+    {
+        print("Equip item : " + itemCurrentlySelected.name);
+        CloseActionPanel();
+    }
+
+    public void DropActionButton()
+    {
+        GameObject instantiatedItem = Instantiate(itemCurrentlySelected.prefab);
+        instantiatedItem.transform.position = dropPoint.position;
+        content.Remove(itemCurrentlySelected);
+        RefreshContent();
+        CloseActionPanel();
+    }
+
+    public void DestroyActionButton()
+    {
+        content.Remove(itemCurrentlySelected);
+        RefreshContent();
+        CloseActionPanel();
+    }
+
 }
